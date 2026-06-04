258f65c32c848880b89a92e12ecce1888cf243e6
scripmushroom-source <scripmushroom@gmail.com>
2026-06-04 22:28:34 +1000

    Update KeySystem.lua

--- KeySystem.lua
+++ KeySystem.lua
@@ -23,7 +23,7 @@ local Config = {
     Secret          = "Antidisestbalishmentarianism", -- This makes the script ONLY run from the key script. Even if they copy the original obfuscated script to bypass the key, they won't be able to!
     
     -- [3] Scripts & Links
-    MainScriptURL   = "https://raw.githubusercontent.com/scripmushroom-source/split-or-steal-script/refs/heads/main/scrpmsos", -- The raw URL of your main script
+    MainScriptURL   = "https://raw.githubusercontent.com/scripmushroom-source/split-or-steal-script/refs/heads/main/scrpmsos.lua", -- The raw URL of your main script
     
     -- [4] Social Media Settings (Set to true to show, false to hide)
     ShowDiscord     = false,
