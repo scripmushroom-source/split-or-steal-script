55c1b0a26f43daf1841eb9d46f02ea6cb7e46af6
scripmushroom-source <scripmushroom@gmail.com>
2026-06-04 22:36:47 +1000

    Update KeySystem.lua

--- KeySystem.lua
+++ KeySystem.lua
@@ -20,7 +20,7 @@ local Config = {
     PlatoSecret     = "bba57837-1c55-44d1-87b3-6b9e9b9cf9c4", -- Your PlatoBoost Secret Key
 
     -- [2] Anti-Bypass / Global Secret Variable
-    Secret          = "Antidisestbalishmentarianism", -- This makes the script ONLY run from the key script. Even if they copy the original obfuscated script to bypass the key, they won't be able to!
+    Secret          = "Antidisestablishmentarianism", -- This makes the script ONLY run from the key script. Even if they copy the original obfuscated script to bypass the key, they won't be able to!
     
     -- [3] Scripts & Links
     MainScriptURL   = "https://raw.githubusercontent.com/scripmushroom-source/split-or-steal-script/refs/heads/main/scrpmsos.lua", -- The raw URL of your main script
